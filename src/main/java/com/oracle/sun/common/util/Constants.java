dde9eaceaba3abc1ebedc0309d456bbc9f7b23bd
disanshijie <878455649@qq.com>
2018-06-05 18:22:13 +0800

    第二次低价

---
+++ src/main/java/com/oracle/sun/common/util/Constants.java
@@ -0,0 +1,10 @@
+package com.oracle.sun.common.util;
+
+public class Constants {
+
+	//项目路径 eg:D:\sunjinchao\data\eclipse\WorkSpace\Space4\project_copyfiles
+	public static String PROJECTPATH=System.getProperty("user.dir");
+	
+	public static String BASEPROPERTY="/src/main/resource/params.properties";
+	
+}
