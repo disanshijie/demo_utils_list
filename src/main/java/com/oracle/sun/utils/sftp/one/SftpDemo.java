1caf02b14c8d46515485d89141b531f6ffd03ca4
disanshijie <878455649@qq.com>
2018-06-19 18:38:01 +0800

    给他是如何

---
+++ src/main/java/com/oracle/sun/utils/sftp/one/SftpDemo.java
@@ -0,0 +1,66 @@
+package com.oracle.sun.utils.sftp.one;
+
+import java.io.File;
+
+import com.jcraft.jsch.ChannelSftp;
+
+public class SftpDemo {
+
+	
+	 //测试
+    public static void main(String[] args) throws Exception {
+    	//test1();
+    	test2();
+    	//test3();
+    }
+    /**
+     * OK
+     * @Description
+     * 返回类型 void
+     * @throws Exception
+     * @注	
+     */
+    public static void test1() throws Exception {
+    	 ChannelSftp sftp = SftpUtil.getSftpConnect("59.110.224.8", 22, "root", "sjc@7ZXJPDZ");
+         String pathString = "D:\\sunjinchao\\垃圾箱/new33.txt";
+         String dir = "D:\\opt\\tomcat-7.0";
+         
+         String directory = "/usr/games";
+         System.out.println("上传文件开始...");
+         //上传文件
+        // SftpUtil.uploadFile(new File(pathString), directory, sftp);
+         //上传文件夹
+         SftpUtil.uploadFile(new File(dir), directory, sftp);
+         
+         //上传文件，指定Linux上文件名称
+         SftpUtil.uploadFile(pathString, directory,"sql.zip", sftp);
+         
+         SftpUtil.exit(sftp);
+         System.exit(0);
+    }
+    /**
+     * OK
+     * @Description
+     * 返回类型 void
+     * @throws Exception
+     * @注	
+     */
+    public static void test2() throws Exception {
+    	 ChannelSftp sftp = SftpUtil.getSftpConnect("59.110.224.8", 22, "root", "sjc@7ZXJPDZ");
+         String pathString = "/usr/games/比赛默认图.zip";
+         //删除文件
+         SftpUtil.rmFile(pathString, sftp);
+         
+         //递归删除目录下的内容
+         String pathString2 = "/usr/games/";
+         SftpUtil.rmDirFile(pathString2, sftp);
+         
+         //递归删除目录
+         //SftpUtil.rmDir(pathString2, sftp, true);
+         
+         SftpUtil.exit(sftp);
+         System.exit(0);
+    }
+    //上传文件
+   
+}
